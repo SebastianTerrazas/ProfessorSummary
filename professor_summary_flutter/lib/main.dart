af766ff6cf1ce9807cb120b91e06b64e82c7b6ca
Sebastian Terrazas <terrazas.sebastian@hotmail.com>
2020-03-04 16:38:50 -0700

    Update main.dart

--- professor_summary_flutter/lib/main.dart
+++ professor_summary_flutter/lib/main.dart
@@ -6,7 +6,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp( 
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
